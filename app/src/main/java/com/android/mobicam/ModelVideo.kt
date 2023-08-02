8e1361e58a98774d948ba70082de6b888c816a6d
Azeem Ali <azeemzaidi86@gmail.com>
2023-08-03 02:31:30 +0500

    player_video & ModelVideo.kt added

---
+++ app/src/main/java/com/android/mobicam/ModelVideo.kt
@@ -0,0 +1,19 @@
+package com.android.mobicam
+
+class ModelVideo {
+    var id: String? = null
+    var title: String? = null
+    var timestamp: String? = null
+    var videoUri: String? = null
+
+    constructor() {
+        //firebase requires empty constructor
+    }
+
+    constructor(id: String?, title: String?, timestamp: String?, videoUri: String?,) {
+        this.id = id
+        this.title = title
+        this.timestamp = timestamp
+        this.videoUri = videoUri
+    }
+}
