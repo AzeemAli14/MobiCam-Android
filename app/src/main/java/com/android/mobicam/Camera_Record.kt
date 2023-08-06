fa3a3de56a0ac3a1ac867ee7b43a9469716e293a
Azeem Ali <azeemzaidi86@gmail.com>
2023-08-07 03:05:54 +0500

    Camera Record Activity Added

---
+++ app/src/main/java/com/android/mobicam/Camera_Record.kt
@@ -0,0 +1,11 @@
+package com.android.mobicam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Camera_Record : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera_record)
+    }
+}
